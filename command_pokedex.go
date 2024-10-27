98a1a166e599735da2557609e341869e362ec333
Ivan <ivan.yaremko@gmail.com>
2024-10-27 12:03:05 +0000

    Update command_pokedex.go

--- command_pokedex.go
+++ command_pokedex.go
@@ -11,7 +11,6 @@ func commandPokedex(cfg *config, args ...string) error {
 	if len(collection) == 0 {
 		return errors.New("no pokemon in collection")
 	}
-
 	fmt.Println("Your pokedex:")
 	for _, pokemon := range collection {
 		fmt.Println("	-", pokemon.Name)
