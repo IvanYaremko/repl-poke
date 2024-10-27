7956930c7d2a8ac4fc55a647dc08af9ae5fac8ba
Ivan <ivan.yaremko@gmail.com>
2024-10-27 12:02:19 +0000

    Update command_catch.go

--- command_catch.go
+++ command_catch.go
@@ -20,10 +20,8 @@ func commandCatch(cfg *config, args ...string) error {
 	if err != nil {
 		return fmt.Errorf("command catch response error: %w", err)
 	}
-
 	base := rand.IntN(pokemon.BaseExperience)
 	target := float64(pokemon.BaseExperience) * 0.33
-
 	fmt.Printf("throwing a ball at %s...\n", pokemon.Name)
 	if base > int(target) {
 		message := fmt.Sprintf("\U0001F386 \U0001F386 \U0001F386 caught: %s \U0001F386 \U0001F386 \U0001F386", pokemon.Name)
